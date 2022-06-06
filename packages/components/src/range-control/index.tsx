35005fa6a94c19b729f9d6ddfcd36558b8dbd5c9
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2022-06-06 11:46:41 +1000

    Remove role=img from Icons and revert to using querySelector
    See: https://github.com/WordPress/gutenberg/pull/41445#issuecomment-1145853777

--- packages/components/src/range-control/index.tsx
+++ packages/components/src/range-control/index.tsx
@@ -229,7 +229,7 @@ function UnforwardedRangeControl< IconProps = unknown >(
 			<Root className="components-range-control__root">
 				{ beforeIcon && (
 					<BeforeIconWrapper>
-						<Icon icon={ beforeIcon } role="img" />
+						<Icon icon={ beforeIcon } />
 					</BeforeIconWrapper>
 				) }
 				<Wrapper
@@ -293,7 +293,7 @@ function UnforwardedRangeControl< IconProps = unknown >(
 				</Wrapper>
 				{ afterIcon && (
 					<AfterIconWrapper>
-						<Icon icon={ afterIcon } role="img" />
+						<Icon icon={ afterIcon } />
 					</AfterIconWrapper>
 				) }
 				{ hasInputField && (
